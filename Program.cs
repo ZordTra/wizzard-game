8ce0e935ce39a8c3c489deb69cf9f46634998686
ZordTra <113171151+ZordTra@users.noreply.github.com>
2022-10-21 19:15:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using ConsoleApp1;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Console.Title = "skynet";
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WindowHeight = 40;
+        
+        Wizard wizard01 = new Wizard("Casper", "unexpecto patronum");
+        Console.WriteLine("Write start to start the game:");
+        string start = Console.ReadLine();
+
+        if (start == "start")
+        {
+            if (wizard01.spellSlots < 0)
+            {
+                wizard01.Meditate();
+            }
+            else
+            {
+                wizard01.CastSpell();
+            }
+        }
+        
+        Console.ReadKey();
+     
+
+        
+    }
+}
---
+++ Wizard.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class Wizard
+    {
+        public string name;
+        public string favoritespell;
+        public int spellSlots;
+        public float experience;
+
+        public Wizard(string name, string favoritespell)
+        {
+            this.name = name;
+            this.favoritespell = favoritespell;
+            spellSlots = 2;
+            experience = 0f;
+        }
+
+        public void CastSpell()
+        {
+
+            if (spellSlots > 0)
+            {
+                Console.WriteLine(name + " cast " + favoritespell);
+                spellSlots--;
+                experience += 0.3f;
+
+            }
+            else
+            {
+                Console.WriteLine(name + "no more spellSLots lefft");
+            }
+
+        }
+
+        public void Meditate()
+        {
+            spellSlots++;
+            Console.WriteLine(name + " spellSlots increads to " + spellSlots);
+
+        }
+    }
+}
